42b4d1a57f6f81d396abdfa6ad871cf06e639e2a
Harley Eades <heades@gru.edu>
2016-08-16 10:49:43 -0400

    Changes with Michael.

--- example-hwk/hwk1/AHG.hs
+++ example-hwk/hwk1/AHG.hs
@@ -1,12 +1,12 @@
-module Grader where
+module AHG where
 
 import Test.QuickCheck
     
 -- Type of tests: Double is the point value, and Property is the actual
 -- test case.
-type QTest = (Double, Property)
+type Test = (Double, Property)
 
 -- The double in the return type is the total score, and the list of
 -- results are the results after running each test in the input list.
-runTests :: [QTest] -> (Double, [Result])
+runTests :: [Test] -> (Double, [IO Result])
 runTests = undefined
