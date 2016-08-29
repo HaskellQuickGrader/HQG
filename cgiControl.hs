15b013b1d493b284f4c79d881aa6a923f36bc0fe
michaelto20 <michael.h.townsend@gmail.com>
2016-08-29 15:31:03 -0400

    Aeson parser working, commented out 'message'

--- cgiControl.hs
+++ cgiControl.hs
@@ -9,18 +9,20 @@ import qualified Data.ByteString.Lazy.Char8 as B
 
 cgiMain :: CGI CGIResult
 cgiMain = do
-			--get header and check for secret token authorization
-			header <- requestHeader "X-Gitlab-Token"
-			case header of
-				Nothing -> error "You are not authenticated."
-				Just h -> do 
-							if(h == "eNbbFFBqgBq5TSGdUtWr9gw4WXptmKbKQKp3P8bPAksYyKvx")
-								then do
-									inputs <- getBody
-									user <- parseJSON $ B.pack inputs
-									_ <- liftIO.begin.show $ map name (map author (commits user))
-									output ""
-								else error "You are not authenticated."
+		--get header and check for secret token authorization
+		header <- requestHeader "X-Gitlab-Token"
+		case header of
+		     Nothing -> error "Error."
+		     Just h -> do
+			  if(h == "eNbbFFBqgBq5TSGdUtWr9gw4WXptmKbKQKp3P8bPAksYyKvx")
+			       then do
+			       	    inputs <- getBody
+				    user <- parseJSON $ B.pack inputs
+				    _ <- liftIO.begin.show $ map email (map author (commits user))
+				    output ""
+			  else do
+				_ <- liftIO.begin.show $ "You are not authenticated."
+				output ""
 			 
 			
 
