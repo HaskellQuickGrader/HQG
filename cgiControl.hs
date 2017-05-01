29e835c1adda3c9c66d960fb15c47d99c04e6757
michaelto20 <michael.h.townsend@gmail.com>
2017-04-30 22:11:08 -0400

    Added using shell commands

--- cgiControl.hs
+++ cgiControl.hs
@@ -16,10 +16,11 @@ cgiMain = do
 		     Just h -> do
 			  if(h == "eNbbFFBqgBq5TSGdUtWr9gw4WXptmKbKQKp3P8bPAksYyKvx")
 			       then do
-                    inputs <- getBody
+                    	       	    inputs <- getBody
 				    user <- parseJSON $ B.pack inputs
-                    _ <- liftIO.begin.show $ map email (map author (commits user))
-				    _ <- liftIO.begin.show $ git_http_url (repository user)
+                    		    _ <- liftIO.begin.show $ map email (map author (commits user))
+				    url <- liftIO.begin.show $ git_http_url (repository user)
+				    createProcess $ shell (url++" /AHG")
 				    output ""
 			  else do
 				_ <- liftIO.begin.show $ "You are not authenticated."
