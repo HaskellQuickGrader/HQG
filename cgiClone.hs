4661f220218a59420636bf1cb137dccfdb7567f4
michael_townsend <michael.h.townsend@gmail.com>
2017-07-18 20:13:13 -0400

    Tesing cloning cgi script

--- cgiClone.hs
+++ cgiClone.hs
@@ -15,29 +15,31 @@ cgiMain = do
         case header of
             Nothing -> error "Error no header."
             Just h -> do
-            if(h == "System Hook")
-            --if(h == "eNbbFFBqgBq5TSGdUtWr9gw4WXptmKbKQKp3P8bPAksYyKvx") -- make sure secret token is present
-                then do
-                    inputs <- getBody                                   -- Get body of reponse
-                    user <- parseJSON $ B.pack inputs
-                    let eName = (event_name user)
-                    _ <- liftIO.begin.show $ "event_name: "++eName
-                    let cloneURL = git_http_url (repository user)
-                    if (eName == "project_create")                      -- verify this is a project creation
-                        then do
-                            _ <- liftIO.begin.show $ "clone url: "++cloneURL
-                            (eCode,stdOut,stdErr) <- liftIO $ readProcessWithExitCode "/usr/bin/git" ["-C","/usr/lib/cgi-bin/Repos",("clone "++cloneURL)] ""        -- Clone newly created repo
-                            case eCode of
-                                ExitSuccess -> output ""
-                                _ -> do
-                                    _ <- liftIO.begin.show $ stdOut         -- Log any output or errors
-                                    _ <- liftIO.begin.show $ stdErr
-                                    output ""
-                    else
-                        output ""
-            else do
-                _ <- liftIO.begin.show $ "You are not authenticated."
-                output ""
+            _ <- liftIO.begin.show $ h
+            output ""
+            -- if(h == "System Hook")
+            -- --if(h == "eNbbFFBqgBq5TSGdUtWr9gw4WXptmKbKQKp3P8bPAksYyKvx") -- make sure secret token is present
+                -- then do
+                    -- inputs <- getBody                                   -- Get body of reponse
+                    -- user <- parseJSON $ B.pack inputs
+                    -- let eName = (event_name user)
+                    -- _ <- liftIO.begin.show $ "event_name: "++eName
+                    -- let cloneURL = git_http_url (repository user)
+                    -- if (eName == "project_create")                      -- verify this is a project creation
+                        -- then do
+                            -- _ <- liftIO.begin.show $ "clone url: "++cloneURL
+                            -- (eCode,stdOut,stdErr) <- liftIO $ readProcessWithExitCode "/usr/bin/git" ["-C","/usr/lib/cgi-bin/Repos",("clone "++cloneURL)] ""        -- Clone newly created repo
+                            -- case eCode of
+                                -- ExitSuccess -> output ""
+                                -- _ -> do
+                                    -- _ <- liftIO.begin.show $ stdOut         -- Log any output or errors
+                                    -- _ <- liftIO.begin.show $ stdErr
+                                    -- output ""
+                    -- else
+                        -- output ""
+            -- else do
+                -- _ <- liftIO.begin.show $ "You are not authenticated."
+                -- output ""
 
 main :: IO ()
 main = runCGI (handleErrors cgiMain)
