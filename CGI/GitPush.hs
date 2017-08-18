bc92ee64440cd43634413df5a810aaf88e446ac8
michael_townsend <michael.h.townsend@gmail.com>
2017-08-18 08:45:42 -0400

    Added GitPush for pushing grade report back to repo

---
+++ CGI/GitPush.hs
@@ -0,0 +1,42 @@
+{-# OPTIONS_GHC -fno-warn-tabs #-}
+import Network.CGI 
+import System.Process
+import System.Exit
+import TransferData
+ 
+ 
+gitCommit :: String -> String -> IO ()
+gitCommit commitMessage pathToCommit = do
+        (exitCode,standardOut,standardErr) <- readProcessWithExitCode "/usr/bin/git" ["-C",pathToCommit,"commit", "-a", "-m",commitMessage] ""
+        case exitCode of
+          ExitSuccess -> do
+                    _ <- begin.show $ "Commiting grade report successful"
+                    return ()
+          _ -> do
+                _ <- liftIO.begin.show $ standardOut
+                _ <- liftIO.begin.show $ standardErr
+                return ()
+                
+gitAddGradeReport :: String -> IO ()
+gitAddGradeReport repoFolder = do
+        (exitCode,standardOut,standardErr) <- readProcessWithExitCode "/usr/bin/git" ["-C",repoFolder,"add", "--all"] ""
+        case exitCode of
+          ExitSuccess -> do
+                    _ <- begin.show $ "Git Add successful for student's repo folder"
+                    return ()
+          _ -> do
+                _ <- liftIO.begin.show $ standardOut
+                _ <- liftIO.begin.show $ standardErr
+                return ()
+                
+gitPushGradeReport :: String -> String -> IO ()
+gitPushGradeReport gitUrl repoFolder = do
+        (exitCode,standardOut,standardErr) <- readProcessWithExitCode "/usr/bin/git" ["-C",repoFolder,"push"] ""
+        case exitCode of
+          ExitSuccess -> do
+                    _ <- begin.show $ "Pushing grade report to repo successful"
+                    return ()
+          _ -> do
+                _ <- liftIO.begin.show $ standardOut
+                _ <- liftIO.begin.show $ standardErr
+                return ()
