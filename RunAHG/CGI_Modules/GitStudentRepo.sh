8f67abf5b944847a3378f1add3b50cafeaedab2a
michael_townsend <michael.h.townsend@gmail.com>
2017-09-17 11:28:11 -0400

    Fix bash script

--- RunAHG/CGI_Modules/GitStudentRepo.sh
+++ RunAHG/CGI_Modules/GitStudentRepo.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-REPOPATH = $1
-HWKNUM = $2
-REPOPATH += "Hwk_$HWKNUM"
+REPOPATH=$1
+HWKNUM=$2
+REPOPATH+="Hwk_$HWKNUM"
+#REPOPATH+=$HWKNUM
 echo "Setting git global config username and email" >> output.txt
 echo "Full repo path: $REPOPATH" >> output.txt
-export HOME=/var/www
-echo $HOME >> output.txt
-git config --global user.name "michael"
-git config --global user.email "michael@gmail.com"
-git -C $REPOPATH pull --all
-git -C $REPOPATH checkout solution
+#export HOME=/var/www
+#echo $HOME >> output.txt
+#git config --global user.name "michael"
+#git config --global user.email "michael@gmail.com"
+#git -C $REPOPATH pull --all
+#git -C $REPOPATH checkout solution
 echo "Finished setting git global configs" >> output.txt
-
