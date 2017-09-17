6568730b4017fad976ee81ce35a7eddf52bcdcab
michael_townsend <michael.h.townsend@gmail.com>
2017-09-17 18:17:53 -0400

    Added working directory

--- RunAHG/SetupAHG.hs
+++ RunAHG/SetupAHG.hs
@@ -15,31 +15,53 @@ import CGI_Modules.TransferData
 -- First argument is the Homework Number
 -- Second argument is the path to the student's homework repo folder, this is used
 -- for copying the grade report into that folder, which is then added to their repo for them to see
+-- Third argument is the student's name whose homework is being graded
 
 -- Need to move student's solution in this module and not in AHGTemplate, because AHGTemplate imports 
 -- Hwk1.Hwk1Tests, which imports Hwk1 (student's solutions)
  
 main = do
-    args@(x:y:xs) <- getArgs
-    _ <- begin.show $ " Homework number: "++x++", student's repo path: "++y
-    listUser
-    let ahgHwk = "AHG_Hwk"++x++".hs"
-    let ahgHwkExe = "./AHG_Hwk"++x   -- for Linux usage
-    currentDir <- getCurrentDirectory
-    let homeworkName = "Hwk"++x
-    let reportFolder = "/Hwk/"++homeworkName  -- Report folder and student's solution file have same name
-    _ <- begin.show $ "Current directory in SetupAHG.hs "++ currentDir
+    args@(hwkNum:repoPath:studentName:xs) <- getArgs
+    _ <- begin.show $ " Homework number: "++hwkNum++", "++studentName++"'s repo path: "++repoPath
+    
+    -- Set up local variables
+    let ahgHwk = "AHG_Hwk"++hwkNum++".hs"
+    let ahgHwkExe = "./AHG_Hwk"++hwkNum
+    let homeworkName = "Hwk"++hwkNum
+    let fullRepoPath = repoPath++"/"++"Hwk_"++hwkNum
+    let reportFolder = "/Hwk/"++homeworkName
     let reportFolderPath = currentDir++"/"++reportFolder
-    clearFolder homeworkName reportFolderPath
-    getStudentHwk y homeworkName reportFolderPath
-    makeAHG x ahgHwk currentDir
+    let workingDir = currentDir++"/"++studentName++"/"++homeworkName++"/"
+    
+    currentDir <- getCurrentDirectory
+    setupWorkingDir studentName currentDir homeworkName
+    
+    getStudentHwk fullRepoPath homeworkName reportFolderPath
+    makeAHG hwkNum ahgHwk currentDir
     makeExe ahgHwk currentDir
-    runExe ahgHwkExe y currentDir
+    runExe ahgHwkExe fullRepoPath currentDir
     -- if(exists)
         -- then runExe ahgHwkExe y
         -- else do
-              
-    
+
+
+setupWorkingDir :: String -> String -> String -> IO ()
+setupWorkingDir studentName currentDir hwkFolder = do
+    let workingDir = currentDir++"/"++studentName
+    let hwkTemplateFolder = currentDir++"/"++"Homeworks/"++hwkFolder
+    doesExist <- doesDirectoryExist workingDir  -- Should be false
+    if(doesExist == False)
+        then do
+            (exitCode,stdOut,stdErr) <- readProcessWithExitCode "cp" [hwkTemplateFolder, workingDir] ""
+                case exitCode of
+                    ExitSuccess -> do
+		    		   _ <- begin.show $ "Successfully created working directory"
+                    _ -> do
+                            _ <- begin.show $ "Unsuccessfully created working directory"
+                            _ <- begin.show $ "Standard out: "++ (show stdOut)
+                            _ <- begin.show $ "Standard error: "++(show stdErr)
+        else begin.show $ "Working directory already exists"
+        
 makeAHG :: String -> String -> String -> IO ()
 makeAHG hwkNum  ahgHwk currentDir = do
     _ <- begin.show $ "starting to make AHG"
