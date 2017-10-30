19132e949ff2fe7d96d7c2b75b06b53a21869bbc
Harley Eades <harley.eades@gmail.com>
2017-10-29 23:19:58 -0400

    Integrating the grader with my Gitlab library.
    Also cleaning up some of the code so that it is easier to maintain.

---
+++ Gitlab-Tools/Gitlab/CGIClone/Main.hs
@@ -0,0 +1,30 @@
+module CGIClone.Main where
+
+import Network.CGI 
+import System.IO
+
+-- Debugging
+
+logStr :: String -> IO ()
+logStr inpStr = do outh <- openFile "clone-log.txt" AppendMode
+                   hPutStrLn outh inpStr
+                   hClose outh
+-- End Debugging
+    
+secretKey :: String
+secretKey = "GUC#Wwdk6x!5dtrrnb#8W$p%$wMgMd7xCvr$CNHmy#D%Vf&Ux6"
+
+cgiMain :: CGI CGIResult
+cgiMain = do      
+        headerToken <- requestHeader "X-Gitlab-Token"
+        case headerToken of
+            Nothing -> error "Error no token header."
+            Just ht -> do
+              if(ht == secretKey) 
+              then do headerEvent <- requestHeader "X-Gitlab-Event"
+                      case headerEvent of
+                        Nothing -> error "Error: no event header"
+                        Just "System Hook" -> do inputs <- getBody
+                                                 liftIO.logStr $ inputs
+                                                 output ""
+              else output ""
