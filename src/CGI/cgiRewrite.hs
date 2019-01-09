87a016d54c76811260159c0cc094d23873c0ecaf
Preston Keel <pkeel85@gmail.com>
2019-01-09 09:29:04 -0500

    Work on grader

--- src/CGI/cgiRewrite.hs
+++ src/CGI/cgiRewrite.hs
@@ -1,39 +1,71 @@
+module CGI.CGIRewrite where
 import Network.CGI
 import System.IO
 import SystemHook
+import SystemHook.GroupM
 import Query
 import qualified Data.ByteString.Lazy.Char8 as CH
 import System.Directory
 import System.Process
 
+import CGI.Clone.Clone
+
 secretKey :: String
 secretKey = "GUC#Wwdk6x!5dtrrnb#8W$p%$wMgMd7xCvr$CNHmy#D%Vf&Ux6"
 
-getBranchName :: String -> Int -> String
-getBranchName [] _ = []
-getBranchName (x:xs) slashCount | x == '/' = if(slashCount == 1)
-                                                then xs
-                                                else getBranchName xs (slashCount + 1)
-                                | otherwise = getBranchName xs slashCount
+-- Debugging
+
+logStr :: String -> IO ()
+logStr inpStr = do outh <- openFile "clone-log.txt" AppendMode
+                   hPutStrLn outh inpStr
+                   hClose outh
+-- End Debugging
+
 
 --Checks to see if system hook has a header associated with it
 processHeader :: Maybe String -> CGI CGIResult
 processHeader Nothing = error "Error: no event header"
 processHeader (Just "System Hook") = getBody >>= processSystemHook                                
 
+
+--Here I need to detemine what kind of event happend: Push, Move to Group, etc.
+--Once determined make function call the handel action:Clone repo, run grade report, etc.
 processSystemHook :: String -> CGI CGIResult
 processSystemHook inputs = do
   liftIO.logStr $ inputs
   let d = sysHookDecoder $ CH.pack inputs
+  case d of
+    Left m -> output m
+    Right (Left m) -> case m of
+                       Left m -> undefined
+                       Right m -> undefined
+    
+    Right (Right a) -> case a of
+                      --For push actions (call grading module)
+                      Injl (Injl (Injl (Injl (Injl (Injl (Injl (Injl (Injr p)))))))) -> do
+                                                                                         liftIO.logStr $ "Push action"
+                                                                                         liftIO.logStr $ show p
+                                                                                         output ""
+                      --Move user to group(call clone module)
+                      Injl (Injl (Injl (Injl (Injl (Injl (Injr g)))))) -> do
+                                                                           liftIO.logStr $ "Move user to group"
+                                                                           liftIO.logStr $ show g
+                                                                           output ""
+                       
   liftIO.logStr.show $ d          
   output ""
 
-getHwkNumber :: String -> Int
-getHwkNumber branch = 
-    let hwkNum = getNum
-        where
-            getNum :: String -> String
-            getNum branch =  
+
+
 
 cgiMain :: CGI CGIResult
-cgiMain = undefined 
+cgiMain = do      
+        headerToken <- requestHeader "X-Gitlab-Token"
+        case headerToken of
+            Nothing -> error "Error no token header."
+            Just ht | ht == secretKey -> requestHeader "X-Gitlab-Event" >>= processHeader
+                    | otherwise -> output ""
+
+
+main :: IO ()
+main = runCGI (handleErrors cgiMain)
